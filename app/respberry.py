b1d7a268dc0a78b71b291fc544faef8a5fb19f52
guzvo92 <guzvo92@gmail.com>
2021-12-02 19:20:04 -0600

    MODIFICACIONES para raspberry 7

--- app/respberry.py
+++ app/respberry.py
@@ -3,18 +3,22 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)  #Para usar números de pin de placa y no del procesador
 GPIO.setwarnings(False) #Impedir popups de warnings no importantes en este caso
 
-GPIO.setup(18, GPIO.IN, GPIO.PUD_UP) #Config pin-5(btn) como entrada con resistencia de pull up
-GPIO.setup(24, GPIO.IN, GPIO.PUD_UP) #Config pin-5(btn) como entrada con resistencia de pull up
-GPIO.setup(17, GPIO.IN, GPIO.PUD_UP) #Config pin-5(btn) como entrada con resistencia de pull up
 
-GPIO.setup(27, GPIO.IN)
-GPIO.setup(22, GPIO.IN)
+led = 21
+boton = 16
+GPIO.setup(led, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(boton, GPIO.IN, GPIO.PUD_UP)
 
 while True:
 
-    if GPIO.input(27) == False:
-        print("falso 27 oprimido")
+    if GPIO.input(led) == True:
+        print("falso led oprimido")
+    else:
+        print("nada en el led")
 
-    if GPIO.input(22) == False:
+    if GPIO.input(boton) == True:
         print("falso 22 oprimido")
+    else:
+        print("nada en el but")
 
+GPIO.cleanup()
