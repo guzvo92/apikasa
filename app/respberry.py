fedb76647d28fea47c7790d71ccf42a4bb1db085
guzvo92 <guzvo92@gmail.com>
2021-12-02 19:21:34 -0600

    MODIFICACIONES para raspberry 8

--- app/respberry.py
+++ app/respberry.py
@@ -8,17 +8,21 @@ led = 21
 boton = 16
 GPIO.setup(led, GPIO.IN, GPIO.PUD_UP)
 GPIO.setup(boton, GPIO.IN, GPIO.PUD_UP)
-
+import time
 while True:
 
     if GPIO.input(led) == True:
+        time.sleep(2)
         print("falso led oprimido")
     else:
+        time.sleep(2)
         print("nada en el led")
 
     if GPIO.input(boton) == True:
+        time.sleep(2)
         print("falso 22 oprimido")
     else:
+        time.sleep(2)
         print("nada en el but")
 
 GPIO.cleanup()
