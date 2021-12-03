3cebf56c68ede195a17403f784c94f3fc91e9d76
guzvo92 <guzvo92@gmail.com>
2021-12-02 20:03:52 -0600

    MODIFICACIONES para raspberry 12

--- app/respberry.py
+++ app/respberry.py
@@ -5,16 +5,24 @@ GPIO.setmode(GPIO.BCM) #Simplemente nos sirve para usar números de pin de placa
 GPIO.setwarnings(False) #Con esto impedimos que nos aparezcan warnings que en este caso no serán importantes
 
 led = 21 #Variable donde ponemos el pin que usaremos para el LED
-btn = 16 ##Variable donde ponemos el pin que usaremos para el pulsador
+btn1 = 6 ##Variable donde ponemos el pin que usaremos para el pulsador
+btn2 = 12 ##Variable donde ponemos el pin que usaremos para el pulsador
 
 
 GPIO.setup(led, GPIO.OUT) #Configuramos el pin 21 (led) como salida
-GPIO.setup(btn, GPIO.IN, GPIO.PUD_UP) #Configuramos el pin 13 (btn) como entrada con resistencia de pull up
+GPIO.setup(btn1, GPIO.IN, GPIO.PUD_UP)
+GPIO.setup(btn2, GPIO.IN, GPIO.PUD_UP) #Configuramos el pin 13 (btn) como entrada con resistencia de pull up
 
 while True:
 
-    if GPIO.input(btn) == False:
-        print ("Pulsador está pulsado")
+    if GPIO.input(btn1) == False:
+        print ("Pulsador1 está pulsado")
+        tiempo = 0.1
+    else:
+        tiempo = 1;
+    
+    if GPIO.input(btn2) == False:
+        print ("Pulsador2 está pulsado")
         tiempo = 0.1
     else:
         tiempo = 1;
