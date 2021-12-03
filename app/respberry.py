5c86369243bbaa86e453d52aff0be29217375d75
guzvo92 <guzvo92@gmail.com>
2021-12-02 18:55:53 -0600

    MODIFICACIONES para raspberry 2

---
+++ app/respberry.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)  #Para usar números de pin de placa y no del procesador
+GPIO.setwarnings(False) #Impedir popups de warnings no importantes en este caso
+
+GPIO.setup(18, GPIO.IN, GPIO.PUD_UP) #Config pin-5(btn) como entrada con resistencia de pull up
+
+if GPIO.input(18) == True:
+    print("falso 18 oprimido")
+
+if GPIO.input(17) == True:
+    print("falso 17 oprimido")
+
+if GPIO.input(24) == True:
+    print("falso 24 oprimido")
