5909a19be6142fe68d40afa250e5b1e73887dd4c
guzvo92 <guzvo92@gmail.com>
2021-12-02 18:57:52 -0600

    MODIFICACIONES para raspberry 3

--- app/respberry.py
+++ app/respberry.py
@@ -4,6 +4,8 @@ GPIO.setmode(GPIO.BCM)  #Para usar números de pin de placa y no del procesador
 GPIO.setwarnings(False) #Impedir popups de warnings no importantes en este caso
 
 GPIO.setup(18, GPIO.IN, GPIO.PUD_UP) #Config pin-5(btn) como entrada con resistencia de pull up
+GPIO.setup(24, GPIO.IN, GPIO.PUD_UP) #Config pin-5(btn) como entrada con resistencia de pull up
+GPIO.setup(17, GPIO.IN, GPIO.PUD_UP) #Config pin-5(btn) como entrada con resistencia de pull up
 
 if GPIO.input(18) == True:
     print("falso 18 oprimido")
